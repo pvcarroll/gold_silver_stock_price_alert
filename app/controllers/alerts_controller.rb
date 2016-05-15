84afd1c5578dc9243fb5a4e2dbc3c13d187fac01
paulcarroll <pvcarroll@gmail.com>
2016-05-15 11:22:41 -0500

    quandl

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,7 @@
 class AlertsController < ApplicationController
   def index
     @alert = Alert.new
+    @alert.get_gold_price
   end
 
   def create
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,2 +1,7 @@
 class Alert < ApplicationRecord
+
+  def get_gold_price
+    require 'quandl'
+    puts Quandl::Dataset.get('WIKI/AAPL')
+  end
 end
