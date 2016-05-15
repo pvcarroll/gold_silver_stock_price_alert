23f736040a4dd716021e2f540d851ddf6892e16f
paulcarroll <pvcarroll@gmail.com>
2016-05-15 17:53:04 -0500

    silver price, alert form

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,7 +2,11 @@ class AlertsController < ApplicationController
   def index
     @alert = Alert.new
     gold_data = @alert.get_gold_price
-    puts gold_data.date, gold_data.usd_am, gold_data.usd_pm
+    @date = gold_data.date
+    @gold_am = gold_data.usd_am
+    @gold_pm = gold_data.usd_pm
+    silver_data = @alert.get_silver_price
+    @silver_price = silver_data.usd
   end
 
   def create
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,7 +1,15 @@
 class Alert < ApplicationRecord
+  QUANDL_API_KEY = 'Rj98VdAhWedxFFbvUCTJ'
 
   def get_gold_price
     require 'quandl'
+    Quandl::ApiConfig.api_key = QUANDL_API_KEY
     return  Quandl::Dataset.get('LBMA/GOLD').data.first
+  end
+
+  def get_silver_price
+    require 'quandl'
+    Quandl::ApiConfig.api_key = QUANDL_API_KEY
+    return Quandl::Dataset.get('LBMA/SILVER').data.first
   end
 end
