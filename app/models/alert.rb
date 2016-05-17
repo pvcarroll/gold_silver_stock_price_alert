62101e8936e132bf35e51351759bca1d8679373b
paulcarroll <pvcarroll@gmail.com>
2016-05-16 21:40:30 -0500

    Alert.check_target_values

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,9 +1,24 @@
 class Alert < ApplicationRecord
   QUANDL_API_KEY = 'Rj98VdAhWedxFFbvUCTJ'
 
-  def get_gold_silver_data
+  def self.get_gold_silver_data
     require 'quandl'
     Quandl::ApiConfig.api_key = QUANDL_API_KEY
     return  {gold_data: Quandl::Dataset.get('LBMA/GOLD').data.first, silver_data: Quandl::Dataset.get('LBMA/SILVER').data.first}
+  end
+
+  def self.check_target_values
+    data = self.get_gold_silver_data
+    gold_price = data[:gold_data].usd_am
+    silver_price = data[:silver_data].usd
+    Alert.all.each do |alert|
+      price = (alert[:item] == 'gold') ? gold_price : silver_price
+
+      if alert[:above_or_below] && (price > alert[:target_value])
+        puts "#{alert[:item]} ABOVE"
+      elsif !alert[:above_or_below] && (price < alert[:target_value])
+        puts "#{alert[:item]} BELOW"
+      end
+    end
   end
 end
