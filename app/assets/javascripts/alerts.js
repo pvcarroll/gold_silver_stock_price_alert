b2465c016645f45a159dc17c1529466b3b69cb57
paulcarroll <pvcarroll@gmail.com>
2016-06-08 11:29:10 -0500

    alerts.js self-invoking function

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,7 +1,12 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+(function () {
+    alert('alerts.js self-invoking function');
+})();
+
 alert('alerts.js message');
+
 
 $(document).ready(function() {
   $("#alert_item").change(function() {
