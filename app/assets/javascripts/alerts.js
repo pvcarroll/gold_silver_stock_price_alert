5b925b50b512766eef34d26d18ef76afca89b2ec
paulcarroll <pvcarroll@gmail.com>
2016-06-08 19:51:07 -0500

    restore js_compressor :uglifier

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,12 +1,8 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-(function () {
-    alert('alerts.js self-invoking function');
-})();
 
 alert('alerts.js message');
-
 
 $(document).ready(function() {
   $("#alert_item").change(function() {
