96d0b9dd96d604f0031529606ea2e8a0383f5f73
paulcarroll <pvcarroll@gmail.com>
2016-06-20 11:01:53 -0500

    fix stock dropdown - open instead on toggle on change

--- app/assets/javascripts/components/FilteredList.js.jsx
+++ app/assets/javascripts/components/FilteredList.js.jsx
@@ -1,6 +1,33 @@
-var stocks = ['GOOG/PINK_LAWEQ,Law Enforcem Assoc (LAWEQ)',
-    'GOOG/PINK_WIPSM,Wisconsin Public Service Corporation (WIPSM)',
-    'GOOG/NYSE_IRF,International Rectifier Corporation (IRF)'];
+var stocks = ["GOOG/PINK_LAWEQ,Law Enforcem Assoc (LAWEQ)",
+    "GOOG/PINK_WIPSM,Wisconsin Public Service Corporation (WIPSM)",
+    "GOOG/NYSE_IRF,International Rectifier Corporation (IRF)",
+    "GOOG/SGX_5SS,Harry S Hldg Ltd. (5SS)",
+    "GOOG/PINK_VBDG,Vertical Branding Inc (VBDG)",
+    "GOOG/NASDAQ_GTSI,Unicom Government Inc (GTSI)",
+    "GOOG/OTC_DKIN,Drucker Inc (DKIN)",
+    "GOOG/STO_SCH_O,Schibsted (SCH-O)",
+    "GOOG/NASDAQ_MAPP,MAP Pharmaceuticals Inc. (MAPP)",
+    "GOOG/SHE_000829,Telling Telecom Holding Co., Ltd. (000829)",
+    "GOOG/NYSE_GRT_F,Glimcher Rlty Pfd F (GRT-F)",
+    "GOOG/PINK_CDARF,CANADIAN ARROW MINES (CDARF)",
+    "GOOG/PINK_ISEYF,WI2WI CORP (ISEYF)",
+    "GOOG/NASDAQ_MCBK,Madison County Financial Inc (MCBK)",
+    "GOOG/OTCBB_TNIX,Telanetix Inc (TNIX)",
+    "GOOG/NASDAQ_CBEY,Cbeyond, Inc. (CBEY)",
+    "GOOG/EPA_ALNEO,Neotion SA (ALNEO)",
+    "GOOG/PINK_TAMB,Tamalpais Bancorp (TAMB)",
+    "GOOG/NASDAQ_NIHD,NII Holdings Inc (NIHD)",
+    "GOOG/NASDAQ_SONO,Fujifilm Sonosite Inc (SONO)",
+    "GOOG/PINK_TRUHF,TRULY INT'L HOLDINGS (TRUHF)",
+    "GOOG/NASDAQ_DBLE,Double Eagle Petroleum Co (DBLE)",
+    "GOOG/OTC_TRNT,Transnet Corp (TRNT)",
+    "GOOG/OTC_MNLU,Mainland Resources (MNLU)",
+    "GOOG/BVMF_SGEN3,Sergen Servicos Gerais de Engenharia SA (SGEN3)",
+    "GOOG/NASDAQ_LOOP,LoopNet, Inc. (LOOP)",
+    "GOOG/PINK_CECX,China Executive Ed (CECX)",
+    "GOOG/STO_COIC,Concentric AB (COIC)",
+    "GOOG/EPA_OROS,Orosdi SCA (OROS)",
+    "GOOG/NSE_GLORY,Glory Polyfilms Limited (GLORY)"];
 
 var FilteredList = React.createClass({
 
@@ -12,7 +39,7 @@ var FilteredList = React.createClass({
         });
         this.setState({items: updatedList});
 
-        $('.dropdown-toggle').dropdown('toggle');
+        $('.dropdown').addClass('open');
     },
 
     getInitialState: function() {
